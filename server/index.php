f1bc1f0283c4ec4437e90dabae1bb3e4c0c6fdf0
mr khalid <81389901+bash-sh900@users.noreply.github.com>
2022-04-11 16:50:53 +0000

    Add files via upload

---
+++ server/index.php
@@ -0,0 +1,23 @@
+<?php
+include("startdb.php");
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>morocco-lab-v1</title>
+	</head>
+	<body>
+		<h2>Morocco-lab-v1</h2>
+		<a href="http://0.0.0.0:4444" >bWAPP</a>
+		<br>
+		<a href="http://0.0.0.0:3333" >xvwa</a>
+		<br>
+		<a href="http://0.0.0.0:5555" >DVWA</a>
+		<br>
+		<a href="mutillidae"">mutillidae</a>
+		<br>
+		<a href="http://0.0.0.0:2222"> hackademic</a>
+		<br>
+		<a href="http://0.0.0.0:1111"> sqli-labs-php7 </a>
+</body>
+</html>
